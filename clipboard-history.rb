9ce4fab144b42329b5b8a1515570f7f864020fe0
Dawn <jiaxin.duan@psi.ch>
2026-04-28 08:42:04 +0200

    update sha256

--- clipboard-history.rb
+++ clipboard-history.rb
@@ -2,7 +2,7 @@ class ClipboardHistory < Formula
   desc "Clipboard history manager with daemon and hotkey support"
   homepage "https://github.com/jdawnduan/clipboard_history"
   url "https://github.com/jdawnduan/clipboard_history/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "b309d86fb390ca7ff438e6699005ad0bf0d8541908aed6c783dadd56173c463a"
   license "MIT"
 
   depends_on "rust" => :build
